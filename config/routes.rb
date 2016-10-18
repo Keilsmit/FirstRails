8aa0769a3c72229c6eee4c4d1b9cfe137221a493
keilsmit <keilsmit@yahoo.com>
2016-10-18 16:44:25 -0400

    Adventure mode

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   get '/' => 'welcome#index'
   get '/billy/(:thing)' => 'welcome#show'
-  get '/lorem/:lorem_type' => 'welcome#lorem'
+  get '/lorem/:lorem_type/(:paragraph)' => 'welcome#lorem'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
